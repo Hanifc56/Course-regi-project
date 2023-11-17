6d901acc8fa1ee2c270e16a8d6b98d907b8c1b3a
Hanifc56 <abuhanif007sd@gmail.com>
2023-11-18 02:34:26 +0600

    list style updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,8 +4,8 @@ const Cart = ({ cart }) => {
   const { course_title } = cart;
   return (
     <div>
-      <ol className="list-item">
-        <li>{course_title}</li>
+      <ol className="list-disc pl-4">
+        <li> {course_title}</li>
       </ol>
     </div>
   );
